ac954f51b3e9077104c3e4433c864a8728c5596c
Nóra Szepes <nora.szepes@tenforce.com>
2017-02-20 14:39:05 +0100

    enabled menu mixin in menu-bar

--- app/components/menu-bar.js
+++ app/components/menu-bar.js
@@ -1,6 +1,7 @@
 import Ember from 'ember';
+import MenuHelper from '../mixins/menu-helper'
 
-export default Ember.Component.extend({
+export default Ember.Component.extend(MenuHelper, {
   userRightLabels: [{
     value: 'Admin',
     key: 'admin'
