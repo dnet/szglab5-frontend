7ebf1f62294e6159d016fde5f1db38ef4bb37610
Nóra Szepes <nora.szepes@tenforce.com>
2017-02-20 11:03:11 +0100

    removed unsued tests coffee files from helpers, mixins and routes

---
+++ tests/unit/helpers/get-array-element-test.js
@@ -0,0 +1,12 @@
+
+import { getArrayElement } from 'szglab5-frontend/helpers/get-array-element';
+import { module, test } from 'qunit';
+
+module('Unit | Helper | get array element');
+
+// Replace this with your real tests.
+test('it works', function(assert) {
+  let result = getArrayElement([42]);
+  assert.ok(result);
+});
+
