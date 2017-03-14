cca592e48f0623d94170e1275d30eec947d05f89
Csutorás Robin <csutorasr@hotmail.com>
2017-03-14 15:40:11 +0100

    error in model

--- app/models/language.js
+++ app/models/language.js
@@ -2,5 +2,5 @@ import DS from 'ember-data';
 
 export default DS.Model.extend({
     name: DS.attr('string'),
-    tests: DS.hasMany('question', {inverse: 'language'})
+    tests: DS.hasMany('test', {inverse: 'language'})
 });
