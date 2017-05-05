684c594bc88e3f24a57542bdb870d01272ef56f8
Csutorás Robin <csutorasr@hotmail.com>
2017-05-05 16:26:28 +0200

    config must be moved to separated files

--- config/environment.js
+++ config/environment.js
@@ -47,30 +47,5 @@ module.exports = function(environment) {
     refreshLeeway: 300 // Refresh the token 5 minutes (300s) before it expires.
   };
 
-  if (environment === 'development') {
-    // ENV.APP.LOG_RESOLVER = true;
-    // ENV.APP.LOG_ACTIVE_GENERATION = true;
-    // ENV.APP.LOG_TRANSITIONS = true;
-    // ENV.APP.LOG_TRANSITIONS_INTERNAL = true;
-    // ENV.APP.LOG_VIEW_LOOKUPS = true;
-  }
-
-  if (environment === 'test') {
-    // Testem prefers this...
-    ENV.baseURL = '/';
-    ENV.locationType = 'none';
-
-    // keep test console output quieter
-    ENV.APP.LOG_ACTIVE_GENERATION = false;
-    ENV.APP.LOG_VIEW_LOOKUPS = false;
-
-    ENV.APP.rootElement = '#ember-testing';
-  }
-
-  if (environment === 'production') {
-      // ENV.location = 'hash';
-      ENV.rootURL = '/~szepes'
-  }
-
   return ENV;
 };
