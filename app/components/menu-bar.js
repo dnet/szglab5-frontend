3084ee8d1e2037e23f1988adda9553f2ee00d5ed
Csutorás Robin <csutorasr@hotmail.com>
2017-05-07 02:34:43 +0200

    menu roleselect removed

--- app/components/menu-bar.js
+++ app/components/menu-bar.js
@@ -5,23 +5,6 @@ import jwt_decode from 'npm:jwt-decode';
 
 export default Ember.Component.extend(MenuHelper, {
   session: Ember.inject.service('session'),
-  userRightLabels: [{
-    value: 'Admin',
-    key: 'admin'
-  }, {
-    value: 'Hallgató',
-    key: 'student'
-  }, {
-    value: 'Javító',
-    key: 'evaluator'
-  }, {
-    value: 'Demonstrátor',
-    key: 'demonstrator'
-  }],
-  currentRight: {
-    value: 'Hallgató',
-    key: 'student'
-  },
   userData: {},
   init() {
     this._super(...arguments);
@@ -35,22 +18,9 @@ export default Ember.Component.extend(MenuHelper, {
     var token = this.get('session.data.authenticated.token');
     if (!Ember.isEmpty(token)) {
       this.set('userData', jwt_decode(token));
-      //TODO: will be replaced after it become the part of the model.
-      var LUT = {
-        ADMIN: 0,
-        STUDENT: 1,
-        CORRECTOR: 2, // EVALUATOR
-        DEMONSTRATOR: 3
-      };
-      if (this.get('userData.role'))
-        this.set('currentRight',this.get('userRightLabels')[LUT[this.get('userData.role')]]);
     }
   },
   actions: {
-    changeUserRight: function (right) {
-      this.set('currentRight', right);
-      return false;
-    },
     toggleMenu() {
       this.set('isMenuNotOpen', !this.get('isMenuNotOpen'));
       return false;
--- app/mixins/menu-helper.js
+++ app/mixins/menu-helper.js
@@ -1,56 +1,58 @@
 import Ember from 'ember';
 
 export default Ember.Mixin.create({
-  studentMenu: [
-    {
-      key: 'student',
-      value: 'Laborok',
-      icon: 'graduation-cap'
-    }
-  ],
-  adminMenu: [
-    {
-      key: 'statistics',
-      value: "Statisztikák",
-      icon: 'bar-chart'
-    }, {
-      key: 'directory',
-      value: "Felhasználók",
-      icon: 'users'
-    }, {
-      key: 'notification',
-      value: "Hirdetmények",
-      icon: 'bell-o'
-    }, {
-      key: 'dashboard',
-      value: "Mérések",
-      icon: 'dashboard'
-    }, {
-      key: 'entry-test',
-      value: "Beugrók",
-      icon: 'pencil'
-    }, {
-      key: 'sql',
-      value: "SQL",
-      icon: 'terminal'
-    }
-  ],
-  demonstratorMenu: [
-    {
-      key: 'demonstrator',
-      value: "Demonstrátor",
-      icon: 'tasks'
-    }, {
-      key: 'entry-test',
-      value: "Beugrók",
-      icon: 'pencil'
-    }
-  ],
-  evaluatorMenu: [
-    {
-      key: 'evaluator',
-      value: "Javító",
-      icon: 'book'
-    }
-  ]
+  menu: {
+    STUDENT: [
+      {
+        key: 'student',
+        value: 'Laborok',
+        icon: 'graduation-cap'
+      }
+    ],
+    ADMIN: [
+      {
+        key: 'statistics',
+        value: "Statisztikák",
+        icon: 'bar-chart'
+      }, {
+        key: 'directory',
+        value: "Felhasználók",
+        icon: 'users'
+      }, {
+        key: 'notification',
+        value: "Hirdetmények",
+        icon: 'bell-o'
+      }, {
+        key: 'dashboard',
+        value: "Mérések",
+        icon: 'dashboard'
+      }, {
+        key: 'entry-test',
+        value: "Beugrók",
+        icon: 'pencil'
+      }, {
+        key: 'sql',
+        value: "SQL",
+        icon: 'terminal'
+      }
+    ],
+    DEMONSTRATOR: [
+      {
+        key: 'demonstrator',
+        value: "Demonstrátor",
+        icon: 'tasks'
+      }, {
+        key: 'entry-test',
+        value: "Beugrók",
+        icon: 'pencil'
+      }
+    ],
+    CORRECTOR: [
+      {
+        key: 'evaluator',
+        value: "Javító",
+        icon: 'book'
+      }
+    ]
+  }
 });
