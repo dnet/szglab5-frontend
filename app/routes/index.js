3793a8f9caeaafa413c04440ecba168a372f321d
Csutorás Robin <csutorasr@hotmail.com>
2017-05-17 11:36:21 +0200

    news default route

--- app/routes/index.js
+++ app/routes/index.js
@@ -3,6 +3,6 @@ import Ember from 'ember';
 
 export default Ember.Route.extend({
   beforeModel: function() {
-    return this.transitionTo('login');
+    return this.transitionTo('news');
   }
 });
