2f71d13d463ffd23f91ae0650307da841f3369bc
Csutorás Robin <csutorasr@hotmail.com>
2017-05-19 00:33:09 +0200

    jshint

--- app/helpers/dateformat.js
+++ app/helpers/dateformat.js
@@ -1,10 +1,10 @@
 import Ember from 'ember';
 
-export function dateformat([date, ...rest]) {
+export function dateformat([date]) {
   if (date && date.getFullYear) {
     var r = date.getFullYear() + ". " + date.getMonth() + ". " + date.getDate() + ". ";
     r += (date.getHours() < 10) ? "0" + date.getHours() : date.getHours();
-    r += ":"
+    r += ":";
     r += (date.getMinutes() < 10) ? "0" + date.getMinutes() : date.getMinutes();
     return r;
   }
