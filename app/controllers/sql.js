183050d82fb4df35ae03343f01947e1353102cd5
Csutorás Robin <csutorasr@hotmail.com>
2017-07-05 15:25:20 +0200

    sql

--- app/controllers/sql.js
+++ app/controllers/sql.js
@@ -1,9 +1,25 @@
 import Ember from 'ember';
+import config from '../config/environment';
 
 export default Ember.Controller.extend({
+  sqlText: '',
   actions: {
-    goToView: function(key) {
+    goToView(key) {
       this.set('currentView', key);
+      return false;
+    },
+    runQuery() {
+      Ember.$.ajax({
+        type: "POST",
+        url: config.backendUrl + "/sql",
+        data: JSON.stringify({ sqlText: this.get('sqlText') }),
+        contentType: "application/json; charset=utf-8",
+        dataType: "json",
+        success: function (data) { alert(data); },
+        failure: function (errMsg) {
+          alert(errMsg);
+        }
+      });
       return false;
     }
   }
