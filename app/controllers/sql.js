e5c92f641d59cfb66d28db327ae3c4c212651e4f
Csutorás Robin <csutorasr@hotmail.com>
2017-07-27 14:51:27 +0200

    sql works

--- app/controllers/sql.js
+++ app/controllers/sql.js
@@ -3,21 +3,34 @@ import config from '../config/environment';
 
 export default Ember.Controller.extend({
   sqlText: '',
+  error: '',
+  data: [],
   actions: {
     goToView(key) {
       this.set('currentView', key);
       return false;
     },
     runQuery() {
+      let errorMessage = (xhr) => {
+        this.set('error', xhr.responseJSON.errors[0].title);
+      };
+      this.set('error','');
+      this.set('data','');
       Ember.$.ajax({
         type: "POST",
         url: config.backendUrl + "/sql",
         data: JSON.stringify({ sqlText: this.get('sqlText') }),
         contentType: "application/json; charset=utf-8",
+        crossDomain: true,
         dataType: "json",
-        success: function (data) { alert(data); },
-        failure: function (errMsg) {
-          alert(errMsg);
+        success: (data) => {
+          this.set('data', data[0]);
+        },
+        failure: errorMessage,
+        statusCode: {
+          500: errorMessage,
+          404: errorMessage,
+          403: errorMessage,
         }
       });
       return false;
