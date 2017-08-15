5bca7456300ea3c93fa90eeeb983b2e7e2c345e6
Csutorás Robin <csutorasr@hotmail.com>
2017-08-15 13:45:30 +0200

    upload works

--- app/components/student-laboratory-upload.js
+++ app/components/student-laboratory-upload.js
@@ -1,4 +1,23 @@
 import Ember from 'ember';
+import config from '../config/environment';
 
 export default Ember.Component.extend({
+  success: false,
+  error: '',
+  actions: {
+    upload(file) {
+      this.set('success', false);
+      this.set('error', '');
+      file.upload(`${config.backendUrl}/deliverables/${this.get('Deliverable.id')}/upload`).then(() => {
+        this.set('success', true);
+      }, data => {
+        if (data && data.errors && data.errors[0]) {
+          this.set('error', data.errors[0]);
+        }
+        else {
+          this.set('error', 'Unknown error.');
+        }
+      });
+    }
+  }
 });
