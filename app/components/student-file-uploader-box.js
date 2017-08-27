88557a28a1e654b9a1799f8e7a79b406e46317bb
Csutorás Robin <csutorasr@hotmail.com>
2017-08-27 21:55:09 +0200

    all uploader failed

--- app/components/student-file-uploader-box.js
+++ app/components/student-file-uploader-box.js
@@ -1,19 +1,21 @@
 import Ember from 'ember';
-import EmberUploader from 'ember-uploader';
 
-export default EmberUploader.FileField.extend({
-  filesDidChange(files) {
-    const uploader = EmberUploader.Uploader.create({
-      url: this.get('url')
-    });
-
-    if (!Ember.isEmpty(files)) {
-      this.sendAction('started');
-      uploader.upload(files[0]).then(() => {
-        this.sendAction('success');
-      }, data => {
-        this.sendAction('falied', data);
+export default Ember.Component.extend({
+  action: {
+    filesDidChange(files) {
+      // TODO: upload
+      /*const uploader = EmberUploader.Uploader.create({
+        url: this.get('url')
       });
+
+      if (!Ember.isEmpty(files)) {
+        this.sendAction('started');
+        uploader.upload(files[0]).then(() => {
+          this.sendAction('success');
+        }, data => {
+          this.sendAction('falied', data);
+        });
+      }*/
     }
   }
 });
