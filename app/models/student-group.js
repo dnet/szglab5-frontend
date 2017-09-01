1c82432ee721d99459e18a8a845fbb452f6f4605
Nóra Szepes <nora.szepes@tenforce.com>
2017-09-01 22:47:11 +0200

    fixed inverse relationship in student-group

--- app/models/student-group.js
+++ app/models/student-group.js
@@ -3,7 +3,7 @@ import DS from 'ember-data';
 export default DS.Model.extend({
   name: DS.attr('string'),
   language: DS.attr('string'),
-  StudentRegistrations: DS.hasMany('studentRegistration', {inverse: 'StudentGroups'}),
+  StudentRegistrations: DS.hasMany('studentRegistration', {inverse: 'StudentGroup'}),
   Appointments: DS.hasMany('appointment', {inverse: 'StudentGroups'}),
   User: DS.belongsTo('user', {inverse: 'StudentGroup'}),
   Semester: DS.belongsTo('semester', {inverse: 'StudentGroups'}),
