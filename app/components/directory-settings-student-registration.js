cf8da106837b2525aca5a6b47ca441c54bb06fcf
Nóra Szepes <nora.szepes@tenforce.com>
2017-09-01 23:11:20 +0200

    Updated design: new user, student settings

--- app/components/directory-settings-student-registration.js
+++ app/components/directory-settings-student-registration.js
@@ -1,6 +1,8 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
+
+  classNames: ['student-registration'],
   semesterName: Ember.computed(function() {
     return this.get('StudentRegistration.Semester').then(semester => {
       return `${semester.get('academicyear')}/${semester.get('academicterm')}`;
