9bfb4c62b864d16be335a7024a0d023d78258e69
Csutorás Robin <csutorasr@hotmail.com>
2017-09-09 00:29:04 +0200

    generate sheets button added for demonstrator

--- app/components/demonstrator-group.js
+++ app/components/demonstrator-group.js
@@ -1,8 +1,10 @@
 import Ember from 'ember';
 import RSVP from 'rsvp';
+import config from '../config/environment';
 import { dateformat } from '../helpers/dateformat';
 
 export default Ember.Component.extend({
+  session: Ember.inject.service('session'),
   header: ['Name', 'Neptun', 'Exercise', 'Date', 'Place', 'Entry grade', 'Exercise Grade', 'Event grade'],
   rowIndecies: ['userName', 'userNeptun', 'exerciseShortName', 'formattedDate', 'location', 'firstEntryTest.grade', 'firstCorrectableDeliverable.grade', 'grade'],
   body: Ember.computed('currentEventTemplate', function () {
@@ -43,6 +45,21 @@ export default Ember.Component.extend({
   actions: {
     evaluateEvent(event) {
       return this.sendAction('evaluateEvent', event);
+    },
+    generateSheet() {
+      const form = document.createElement('form');
+      form.setAttribute('target', '_blank');
+      form.setAttribute('method', 'post');
+      form.setAttribute('action', `${config.backendUrl}/event-templates/${this.get('currentEventTemplate.id')}/listDownload.zip`);
+      const hiddenInput = document.createElement('input');
+      hiddenInput.setAttribute('type', 'hidden');
+      hiddenInput.setAttribute('name', 'token');
+      hiddenInput.setAttribute('value', this.get('session.data.authenticated.token'));
+      form.appendChild(hiddenInput);
+      document.body.appendChild(form);
+      form.submit();
+      form.remove();
+      return false;
     }
   }
 });
