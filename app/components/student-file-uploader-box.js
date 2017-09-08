02118169fdbb19f48de2a017ee0e7079997bd41c
Csutorás Robin <csutorasr@hotmail.com>
2017-09-08 20:43:23 +0200

    file upload fixed (same file could not be uploaded)

--- app/components/student-file-uploader-box.js
+++ app/components/student-file-uploader-box.js
@@ -22,6 +22,7 @@ export default Ember.Component.extend({
           crossDomain: true,
           success: () => {
             this.sendAction('success');
+            event.target.value = '';
           },
           failure: errorMessage,
           statusCode: {
