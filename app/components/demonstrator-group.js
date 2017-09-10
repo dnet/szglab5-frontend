4b9b504f1b1b38b681e42fad66297379f0a74272
Csutorás Robin <csutorasr@hotmail.com>
2017-09-10 22:59:53 +0200

    included data for demostrator

--- app/components/demonstrator-group.js
+++ app/components/demonstrator-group.js
@@ -4,42 +4,32 @@ import config from '../config/environment';
 import { dateformat } from '../helpers/dateformat';
 
 export default Ember.Component.extend({
+  store: Ember.inject.service(),
   session: Ember.inject.service('session'),
   header: ['Name', 'Neptun', 'Exercise', 'Date', 'Place', 'Entry grade', 'Exercise Grade', 'Event grade'],
-  rowIndecies: ['userName', 'userNeptun', 'exerciseShortName', 'formattedDate', 'location', 'firstEntryTest.grade', 'firstCorrectableDeliverable.grade', 'grade'],
+  rowIndecies: ['StudentRegistration.displayName', 'StudentRegistration.User.neptun', 'ExerciseSheet.ExerciseType.shortName', 'formattedDate', 'location', 'firstEntryTest.grade', 'firstCorrectableDeliverable.grade', 'grade'],
   body: Ember.computed('currentEventTemplate', function () {
     return new RSVP.Promise((resolve, reject) => {
-      this.get('currentEventTemplate.Events').then(events => {
-        const body = [];
-        const promises = [];
-        events.forEach(event => {
-          promises.push(
-            RSVP.Promise.all(
-              [
-                event.get('ExerciseSheet.ExerciseType'),
-                event.get('User')
-              ]
-            ).then(([exerciseType, user]) => {
-              event.set('exerciseShortName', exerciseType.get('shortName'));
-              event.set('userNeptun', user.get('neptun'));
-              event.set('userName', user.get('displayName'));
-              event.set('formattedDate', dateformat([event.get('date')]));
-              body.push(event);
-              return true;
-            }, err => {
-              console.error(err);
-              reject(err);
-            })
-          );
-        });
-        RSVP.Promise.all(promises).then(() => resolve(body), err => {
+      if (this.get('currentEventTemplate.id')) {
+        this.get('store').query('event', {
+          filter: {
+            eventTemplateId: this.get('currentEventTemplate.id')
+          }
+        }).then(events => {
+          const body = [];
+          events.forEach(event => {
+            event.set('formattedDate', dateformat([event.get('date')]));
+            body.push(event);
+          });
+          resolve(body);
+        }, err => {
           console.error(err);
           reject(err);
         });
-      }, err => {
-        console.error(err);
-        reject(err);
-      });
+      }
+      else {
+        resolve([]);
+      }
     });
   }),
   actions: {
