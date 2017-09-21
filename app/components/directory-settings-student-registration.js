b81a44507c6b9a06f2b48706b66ea6d47dc3f271
Csutorás Robin <csutorasr@hotmail.com>
2017-09-21 19:04:08 +0200

    Definalize button added #77

--- app/components/directory-settings-student-registration.js
+++ app/components/directory-settings-student-registration.js
@@ -1,11 +1,25 @@
 import Ember from 'ember';
 
 export default Ember.Component.extend({
-
   classNames: ['student-registration'],
   semesterName: Ember.computed(function() {
     return this.get('StudentRegistration.Semester').then(semester => {
       return `${semester.get('academicyear')}/${semester.get('academicterm')}`;
     });
-  })
+  }),
+  actions: {
+    definalize(deliverable) {
+      deliverable.set('success', false);
+      deliverable.set('error', '');
+      deliverable.set('finalized', false);
+      deliverable.save().then(() => {
+        deliverable.set('success', true);
+      }, (t) => {
+        if (t.errors && t.errors.length > 0 && t.errors[0].title) {
+          deliverable.set('error', t.errors[0].title);
+        }
+      });
+      return false;
+    }
+  }
 });
