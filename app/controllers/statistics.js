bc09ce3f2c8dbbaa3b525ae6aff4a3a07a894da2
Nóra Szepes <nora.szepes@tenforce.com>
2017-10-14 00:55:00 +0200

    Statisztikak: default corrector load

--- app/controllers/statistics.js
+++ app/controllers/statistics.js
@@ -3,6 +3,15 @@ import config from '../config/environment';
 
 export default Ember.Controller.extend({
   session: Ember.inject.service('session'),
+  currentView: 'correctors',
+  selectedMenu: {
+    key: 'correctors',
+    description: 'Javítók terheltsége'
+  },
+  init() {
+    this._super(...arguments);
+    this.loadData({});
+  },
   subMenu: [
     {
       key: 'correctors',
@@ -20,9 +29,36 @@ export default Ember.Controller.extend({
       exerciseTypeNeeded: true
     }
   ],
+
+  loadData: function(params){
+    this.set('data', {});
+    let errorMessage = (xhr) => {
+      this.set('error', xhr.responseJSON.errors[0].title);
+    };
+    this.set('error', '');
+
+    Ember.$.ajax({
+      type: "GET",
+      url: `${config.backendUrl}/statistics/${this.get('currentView')}?${Ember.$.param(params)}`,
+      beforeSend: (xhr) => { xhr.setRequestHeader('Authorization', `Bearer ${this.get('session.data.authenticated.token')}`); },
+      contentType: "application/json; charset=utf-8",
+      crossDomain: true,
+      dataType: "json",
+      success: (data) => {
+        this.set('data', data);
+      },
+      failure: errorMessage,
+      statusCode: {
+        500: errorMessage,
+        404: errorMessage,
+        403: errorMessage,
+      }
+    });
+  },
   actions: {
     goToView(key) {
       this.set('currentView', key);
+      this.set('data', {});
       this.actions.load.apply(this);
       return false;
     },
@@ -37,13 +73,10 @@ export default Ember.Controller.extend({
       return false;
     },
     load() {
-      this.set('data', {});
       const selectedMenu = this.get('subMenu').find(x => x.key === this.get('currentView'));
       this.set('selectedMenu', selectedMenu);
-      let errorMessage = (xhr) => {
-        this.set('error', xhr.responseJSON.errors[0].title);
-      };
-      this.set('error', '');
+      this.set('data', {});
+
       const params = {};
       if (selectedMenu.eventTemplateNeeded) {
         const etid = this.get('selectedEventTemplate.id');
@@ -61,23 +94,7 @@ export default Ember.Controller.extend({
           return false;
         }
       }
-      Ember.$.ajax({
-        type: "GET",
-        url: `${config.backendUrl}/statistics/${this.get('currentView')}?${Ember.$.param(params)}`,
-        beforeSend: (xhr) => { xhr.setRequestHeader('Authorization', `Bearer ${this.get('session.data.authenticated.token')}`); },
-        contentType: "application/json; charset=utf-8",
-        crossDomain: true,
-        dataType: "json",
-        success: (data) => {
-          this.set('data', data);
-        },
-        failure: errorMessage,
-        statusCode: {
-          500: errorMessage,
-          404: errorMessage,
-          403: errorMessage,
-        }
-      });
+      this.loadData(params);
       return false;
     }
   }
